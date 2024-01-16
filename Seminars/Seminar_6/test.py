0a56bb25a78999721daee88f58eed54bcbf17295
Anna <tolstovaaniuta@yandex.ru>
2024-01-16 11:53:35 +0300

    2 task added

---
+++ Seminars/Seminar_6/test.py
@@ -0,0 +1,12 @@
+from my_first_module import module
+
+module.my_module()
+
+def second():
+    print("Second")
+from copy import deepcopy as dp
+
+dp(2, None)
+
+from copy import *
+copy.deepcopy()
---
+++ Seminars/Seminar_6/test_1.py
@@ -0,0 +1,4 @@
+from test import second
+
+def first():
+    print("First")
